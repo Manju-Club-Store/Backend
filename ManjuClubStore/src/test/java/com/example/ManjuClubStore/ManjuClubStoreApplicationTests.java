2d5525237704650301880fe31f04bc3fb3eb7c27
Sungho Lee <dltjdgh69@gmail.com>
2023-05-12 14:17:04 +0900

    [test]: Club 도메인 테스트 구현

--- ManjuClubStore/src/test/java/com/example/ManjuClubStore/ManjuClubStoreApplicationTests.java
+++ ManjuClubStore/src/test/java/com/example/ManjuClubStore/ManjuClubStoreApplicationTests.java
@@ -1,13 +1,22 @@
 package com.example.ManjuClubStore;
 
+import Domain.Club;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.Optional;
 
 @SpringBootTest
 class ManjuClubStoreApplicationTests {
 
 	@Test
-	void contextLoads() {
+	void clubDomain() {
+		Club club = new Club();
+		club.setFee(Optional.ofNullable(20));
+		club.setContactKakao(Optional.ofNullable("dltjdgh97"));
+
+		System.out.println(club.getFee().get());
+		System.out.println(club.getContactKakao().get());
 	}
 
 }
