4b1590035cffd8fb852a382f1ae8df2ad423d983
Sungho Lee <dltjdgh69@gmail.com>
2023-05-12 16:43:38 +0900

    [chore,feat]: Controller, Repository, ClubService 생성 및 일부 구현

---
+++ ManjuClubStore/src/main/java/Service/ClubService.java
@@ -0,0 +1,11 @@
+package Service;
+
+import Repositories.ClubRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ClubService {
+    @Autowired
+    private ClubRepository clubRepository;
+}
