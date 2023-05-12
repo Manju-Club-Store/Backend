a81efd1b48accc7d6e595e3cb6a7bd650998b224
Sungho Lee <dltjdgh69@gmail.com>
2023-05-12 11:09:57 +0900

    [setting]: 초기 프로젝트 생성

---
+++ ManjuClubStore/src/main/java/com/example/ManjuClubStore/ManjuClubStoreApplication.java
@@ -0,0 +1,13 @@
+package com.example.ManjuClubStore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManjuClubStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ManjuClubStoreApplication.class, args);
+	}
+
+}
---
+++ ManjuClubStore/src/test/java/com/example/ManjuClubStore/ManjuClubStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ManjuClubStore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ManjuClubStoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
