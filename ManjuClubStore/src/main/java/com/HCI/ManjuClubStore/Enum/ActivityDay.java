dfb1ed6e8bef83b2e9ca6dbce59eea04bb81b05f
Sungho Lee <dltjdgh69@gmail.com>
2023-05-25 18:45:12 +0900

    [refactor]: enum 클래스에 Jsonproperty 어노테이션 추가
    프론트엔드에서 소문자 enum 값이 들어와도 처리 가능하도록 수정

--- ManjuClubStore/src/main/java/com/HCI/ManjuClubStore/Enum/ActivityDay.java
+++ ManjuClubStore/src/main/java/com/HCI/ManjuClubStore/Enum/ActivityDay.java
@@ -1,11 +1,20 @@
 package com.HCI.ManjuClubStore.Enum;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 public enum ActivityDay {
+    @JsonProperty("monday")
     MONDAY,
+    @JsonProperty("tuesday")
     TUESDAY,
+    @JsonProperty("wednesday")
     WEDNESDAY,
+    @JsonProperty("thursday")
     THURSDAY,
+    @JsonProperty("friday")
     FRIDAY,
+    @JsonProperty("saturday")
     SATURDAY,
+    @JsonProperty("sunday")
     SUNDAY;
 }
--- ManjuClubStore/src/main/java/com/HCI/ManjuClubStore/Enum/ClubSize.java
+++ ManjuClubStore/src/main/java/com/HCI/ManjuClubStore/Enum/ClubSize.java
@@ -1,6 +1,7 @@
 package com.HCI.ManjuClubStore.Enum;
 
 public enum ClubSize {
+
     lessThan10,
     tenTo25,
     twentyFiveTo50,
